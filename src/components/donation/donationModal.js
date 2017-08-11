827f265b0f2143d637660b7de89b234b87bf1c1e
rasitozcan <rasitozcan@Rasits-MacBook-Pro.local>
2017-08-11 09:48:00 +0300

    prettier test

--- src/components/donation/donationModal.js
+++ src/components/donation/donationModal.js
@@ -7,8 +7,8 @@ import Col from 'antd/lib/col';
 
 class DonationModal extends Component {
   constructor(props) {
-    super(props)
-    		this.state = {
+    			super(props);
+    this.state = {
       editMode: false,
       data: props.data,
     };
